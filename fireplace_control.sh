280cf0a3fb899eb5c6f2a1eb50dc712cacf620b5
Piyush Kumar <piyushkumar.jiit@gmail.com>
2025-05-05 17:31:55 -0500

    Update fireplace_control.sh

--- fireplace_control.sh
+++ fireplace_control.sh
@@ -1,13 +1,20 @@
 #! /bin/sh
+device_path='/dev/ttyUSB0'
 
 case "$1" in
   start)
       echo "Starting Fireplace via serial connection to $device_path  ... "
-      echo -n "TX_URH 1,304.20,2,/URH/Start3.proto.xml,2,500,3\r" > /dev/ttyUSB0
+      sudo fuser -k $device_path
+      echo -n "TX_URH 1,304.20,2,/URH/Start3.proto.xml,2,500,3\r" > $device_path
+      sleep 3
+      echo "Started Fireplace."
   ;;
   stop)
     echo "Stopping Fireplace via serial connection to $device_path ..."
-    echo -n "TX_URH 2,304.20,2,/URH/Stop3.proto.xml,2,500,5\r" > /dev/ttyUSB0
+    sudo fuser -k $device_path
+    echo -n "TX_URH 2,304.20,2,/URH/Stop3.proto.xml,2,500,5\r" > $device_path
+    sleep 3
+    echo "Stopped fireplace."
  ;;
   *)
     echo "Usage: $0 {start | stop}"
